21c359a56874ee7713937c0d5cf97327dc12c18d
xecronix <xecronix@yahoo.com>
2025-04-14 20:48:40 -0400

    fixing gitignore and removing some generated files

--- bin/builderzebra.dart
+++
@@ -1,5 +0,0 @@
-import 'package:builderzebra/builderzebra.dart' as builderzebra;
-
-void main(List<String> arguments) {
-  print('Hello world: ${builderzebra.calculate()}!');
-}
--- test/builderzebra_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:builderzebra/builderzebra.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
